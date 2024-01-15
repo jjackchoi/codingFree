4f43cc4e19d45646d05c08cd786ed80b5e45e01c
jjackchoi <jackchoi0325@gmail.com>
2024-01-15 14:40:57 +0900

    롬복기능 빌드, 롬복하고 로깅 사용해서 코드 리팩터링

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class MemberController {
     @Autowired
     private MemberRepository memberRepository;
@@ -19,13 +21,13 @@ public class MemberController {
 
     @PostMapping("/join")
     public String join(MemberForm form){
-        System.out.println(form.toString());
+        log.info(form.toString());
         // 1. dto -> 엔티티
         Member member = form.toEntity();
-        System.out.println(member.toString());
+        log.info(member.toString());
         // 2. 리파지터리로 엔티티를 db에 저장
         Member saved = memberRepository.save(member);
-        System.out.println(saved);
+        log.info(saved.toString());
         return "redirect:/signup";
     }
 }
