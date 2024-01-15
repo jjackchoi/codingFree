7d2af934e94ed833f30e1f233bac67b39fad3e93
jjackchoi <jackchoi0325@gmail.com>
2024-01-15 16:16:04 +0900

    게시글 목록 구현 findAll(엔티티에 기본생성자 추가)

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @Slf4j
@@ -29,5 +33,19 @@ public class MemberController {
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
         return "redirect:/signup";
+    }
+
+    @GetMapping("/members/{id}")
+    public String findById(@PathVariable Long id, Model model){
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String findAll(Model model){
+        ArrayList<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members/index";
     }
 }
