44bf10ca9ac10b49418b6a07168de4b8f08971af
jjackchoi <jackchoi0325@gmail.com>
2024-01-19 17:34:49 +0900

    댓글 구현 중 CommentRepository의 @Query까지 구현(책 408페이지 중간)

--- src/main/java/com/example/firstproject/api/CoffeeApiController.java
+++ src/main/java/com/example/firstproject/api/CoffeeApiController.java
@@ -14,51 +14,7 @@ import java.util.List;
 @RestController
 @Slf4j
 public class CoffeeApiController {
-//    @Autowired
-//    private CoffeeRepository coffeeRepository;
-//    @GetMapping("/api/coffees")
-//    public List<Coffee> index(){
-//        return coffeeRepository.findAll();
-//    }
-//    @GetMapping("/api/coffees/{id}")
-//    public Coffee findById(@PathVariable Long id){
-//        return coffeeRepository.findById(id).orElse(null);
-//    }
-//    @PostMapping("/api/coffees")
-//    public Coffee create(@RequestBody CoffeeForm form){
-//        Coffee coffee = form.toEntity();
-//        return coffeeRepository.save(coffee);
-//    }
-//    @PatchMapping("/api/coffees/{id}")
-//    public ResponseEntity<Coffee> update(@PathVariable Long id, @RequestBody CoffeeForm form){
-//        // 1. dto -> 엔티티
-//        Coffee coffee = form.toEntity();
-//        log.info("id: {}, target: {}", id, coffee.toString());
-//        // 2. 타깃 조회
-//        Coffee target = coffeeRepository.findById(id).orElse(null);
-//        // 3. 잘못된 요청 처리
-//        if(target == null || id != coffee.getId()){
-//            log.info("잘못된 요청! id: {}, target: {}", id, coffee.toString());
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-//        }
-//        // 4. 업데이트 및 정상응답(200) 하기
-//        target.patch(coffee);
-//        Coffee updated = coffeeRepository.save(target);
-//        return ResponseEntity.status(HttpStatus.OK).body(updated);
-//    }
-//    @DeleteMapping("/api/coffees/{id}")
-//    public ResponseEntity<Coffee> delete(@PathVariable Long id){
-//        // 1. 대상 찾기
-//        Coffee target = coffeeRepository.findById(id).orElse(null);
-//        // 2. 잘못된 요청 처리하기
-//        if(target == null){
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-//        }
-//        // 3. 대상 삭제하기
-//        coffeeRepository.delete(target);
-//        return ResponseEntity.status(HttpStatus.OK).build();
-//
-//    }
+
     @Autowired
     private CoffeeService coffeeService;
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,22 @@
+-- 기존 Article 데이터
 INSERT INTO article(title, content) VALUES('가가가가', '1111');
 INSERT INTO article(title, content) VALUES('나나나나', '2222');
 INSERT INTO article(title, content) VALUES('다다다다', '3333');
+INSERT INTO article(title, content) VALUES('당신의 인생 영화는?', '댓글 고');
+INSERT INTO article(title, content) VALUES('당신의 소울 푸드는?', '댓글 고고');
+INSERT INTO article(title, content) VALUES('당신의 취미는?', '댓글 고고고');
+-- 4번 게시글의 댓글 추가
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'PARK', '굿 윌 헌팅');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'KIM', '아이 엠 셈');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, 'CHOI', '쇼생크 탈출');
+-- 5번 게시글의 댓글 추가
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'PARK', '치킨');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'KIM', '피자');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, 'CHOI', '초밥');
+-- 6번 게시글의 댓글 추가
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'PARK', '조깅');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'KIM', '유튜브 시청');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, 'CHOI', '축구');
 
 INSERT INTO member(email, password) VALUES('a@naver.com', '1111');
 INSERT INTO member(email, password) VALUES('b@naver.com', '2222');
@@ -9,3 +25,6 @@ INSERT INTO member(email, password) VALUES('c@naver.com', '3333');
 INSERT INTO coffee(name, price) VALUES('아메리카노', 2500);
 INSERT INTO coffee(name, price) VALUES('라떼', 3000);
 INSERT INTO coffee(name, price) VALUES('초코바나나', 4000);
+
+
+
