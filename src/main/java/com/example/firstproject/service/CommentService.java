84aa9c052b72072c53d45d898b934c436f4224e8
jjackchoi <jackchoi0325@gmail.com>
2024-01-23 16:10:04 +0900

    댓글 구현 중 해당 article의 댓글 생성 rest api로 구현

--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -1,10 +1,13 @@
 package com.example.firstproject.service;
 
 import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.entity.Comment;
 import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,5 +34,19 @@ public class CommentService {
                 .stream() // 댓글 엔티티 목록을 스트림으로 변환
                 .map(comment -> CommentDto.createCommentDto(comment)) // map( a -> b ): 스트림의 각 요소(a)를 꺼내 b를 수행한 결과로 매핑, 즉 엔티티를 dto로 매핑
                 .collect(Collectors.toList()); // 스트림을 리스트로 변환
+    }
+
+    @Transactional
+    public CommentDto create(Long articleId, CommentDto dto) {
+        // 1. 게시글 조회 및 예외 발생
+        Article article = articleRepository.findById(articleId) //부모 게시글 가져오기
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패!" +
+                        "대상 게시글이 없습니다.")); // 없으면 에러 메세지 출력
+        // 2. 댓글 엔티티 생성
+        Comment comment = Comment.createComment(dto, article);
+        // 3. 댓글 엔티티를 db에 저장
+        Comment created = commentRepository.save(comment);
+        // 4. dto를 변환해 반환
+        return CommentDto.createCommentDto(created);
     }
 }
