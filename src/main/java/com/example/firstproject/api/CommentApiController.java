8cb0a47c28704478f2cd4f77fcaa6f221c0bcde3
jjackchoi <jackchoi0325@gmail.com>
2024-01-23 16:39:18 +0900

    댓글 구현 중 댓글 수정 rest api로 구현

--- src/main/java/com/example/firstproject/api/CommentApiController.java
+++ src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -38,7 +38,16 @@ public class CommentApiController {
         // 결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(createDto);
     }
+
     // 3. 댓글 수정
+    @PatchMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> update(@PathVariable Long id,
+                                             @RequestBody CommentDto dto){
+        // 서비스에 위임
+        CommentDto updateDto = commentService.update(id, dto);
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(updateDto);
+    }
     // 4. 댓글 삭제
 
 
--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -49,4 +49,18 @@ public class CommentService {
         // 4. dto를 변환해 반환
         return CommentDto.createCommentDto(created);
     }
+
+    @Transactional
+    public CommentDto update(Long id, CommentDto dto) {
+        // 1. 댓글 조회 및 예외 발생
+        Comment target = commentRepository.findById(id) // 댓글 가져오기
+                .orElseThrow(() -> new IllegalArgumentException("댓글 수정 실패!" +
+                        "댓글이 없습니다")) ; // 없으면 에러 메세지 출력
+        // 2. 댓글 수정
+        target.patch(dto);
+        // 3. db로 갱신
+        Comment updated = commentRepository.save(target);
+        // 4. 댓글 엔티티를 dto로 변환 및 반환
+        return CommentDto.createCommentDto(updated);
+    }
 }
